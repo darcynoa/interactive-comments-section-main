972428d6edaeff4a04b58c0257d3ea8f421f862b
Noah D'Arcy <noahdarcy.dev@gmail.com>
2025-01-24 19:51:48 -0500

    Starting to structure the input component

--- components/CommentInput.tsx
+++ components/CommentInput.tsx
@@ -1,9 +1,20 @@
 /** @format */
 
+import Image from "next/image";
 import React from "react";
 
 function CommentInput() {
-  return <div>CommentInput</div>;
+  return (
+    <div className="fixed mx-auto bottom-[2rem] bg-white rounded-[1rem]">
+      <Image
+        src="/avatars/maxblagun"
+        alt="Profile of Max Blagun"
+        width={50}
+        height={50}
+      />
+      <textarea />
+    </div>
+  );
 }
 
 export default CommentInput;
